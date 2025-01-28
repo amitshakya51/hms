4d4370a17c66ac93da5938c3077dc9ac0669ed43
AMIT SHAKYA <amit.shakya51@gmail.com>
2025-01-28 23:19:46 +0530

    initial commit

---
+++ src/main/java/hospital/management/system/hms/HmsApplication.java
@@ -0,0 +1,13 @@
+package hospital.management.system.hms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HmsApplication.class, args);
+	}
+
+}
